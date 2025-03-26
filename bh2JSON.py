5880fbbe30a4ce6820387a0d68450f8094b4a4ab
QWERTYphish <julien.mansier@gmail.com>
2025-03-26 17:48:05 +0000

    Fixed. COrrectly outputting all behaviors

--- bh2JSON.py
+++ bh2JSON.py
@@ -3,6 +3,11 @@ import os
 import re
 import json
 
+codePattern = r'\[\s*(BH\d+)\s*\]'
+catPattern = r':\s*([^/]+)'
+behPattern = r'\].*?\n\s+(.+)'
+expPattern = r'Explained:(.*?)Prevalence'
+
 def parse_input(input_text):
     # Initialize the result dictionary
     result = []
@@ -15,21 +20,39 @@ def parse_input(input_text):
     # and add to a new list
     for i in range(int(len(sections)/2)):
         temp = {}
+        tempDect = []
         
         sect1 = sections[i*2+1]
         sect2 = sections[i*2+2]
 
         # Get BH Code
-        temp["bhcode"]=""
+        temp["bhcode"]= re.search(codePattern, sect1).group(1)
+
 
         # Get Category
-        temp["category"]=""
+        temp["category"]= re.search(catPattern, sect1).group(1)
 
-        # Get Count
-        temp["count"]=""
+        # Get Behavior
+        temp["behavior"] = re.search(behPattern, sect1).group(1)
 
         # Get Explaination 
-        temp["explaination"]=""
+        tempExplain = re.search(expPattern, sect2, re.DOTALL).group(1)
+        temp["explaination"]= re.sub(r'\s+', ' ', tempExplain)
+
+        # Get Detections
+        if "Detections ---------------------------------------------------------------------" in sect2:
+            detectSection = sect2.split("Detections ---------------------------------------------------------------------")
+
+            lines = detectSection[1].splitlines()
+            pattern = r'^\s*\d+\)\s*(.*)$'
+            for line in lines:
+                match = re.match(pattern, line)
+                if match:
+                    tempDect.append(match.group(1))
+
+            temp["detections"] = tempDect
+        else:
+            temp["detections"] = []
 
 
         result.append(temp)
@@ -66,8 +89,8 @@ def main():
         json_output = json.dumps(parsed_data, indent=2)
 
         # Output JSON output
-        #with open("bh.json", "w") as out:
-            #out.write(json.dumps(json_output))
+        with open("bh.json", "w") as out:
+            out.write(json.dumps(json_output))
 
     except FileNotFoundError:
         print(f"Error: The file '{file_path}' was not found.")
